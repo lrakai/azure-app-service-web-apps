e806bb3e542792f326117341161a458e656f8430
Logan Rakai <lmrakai@gmail.com>
2018-06-13 13:15:45 -0600

    Remove template

--- startup.ps1
+++ startup.ps1
@@ -1,5 +1,4 @@
-$Region = "WestUS2"
+$Region = "EastUS"
 $ResourceGroup = "azure-app-service"
 Login-AzureRmAccount
 New-AzureRmResourceGroup -Name $ResourceGroup -Location $Region
-New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroup -TemplateFile .\infrastructure\arm-template.json -Name "lab"
